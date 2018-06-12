3040e6e01c620561610e39f21b67b1caaf27c60d
Andrey <slaner2008@yandex.ru>
2018-06-12 20:26:47 +0300

    refactoring #1

---
+++ dev/kit.modal.js
@@ -0,0 +1,43 @@
+/**
+ * UI Kit Modal v1.1.0
+ * Copyright 2017-2018 Andrey Ponomarenko
+ * Licensed under  ()
+ */
+
+// ======================= MODEL ============================
+
+if	(!document.kit) document.kit = {};
+if	(!document.kit.modal) document.kit.modal = {};
+document.kit.modal.modalCounter = 0;
+document.kit.modal.modals = [];
+
+class KitModal {
+	constructor(htm) {
+		this.htm = htm;
+	}
+
+	show() {
+
+	}
+
+	hide() {
+
+
+	}
+
+	addTrigger () {
+	//	Добавить активатор модального окна
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
